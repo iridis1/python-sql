965dd1779966a0460782edb5a63c3e3712f668c2
Bouke <bouke@iridis.nl>
2024-12-27 14:30:03 +0100

    Added missing tables.

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -3,6 +3,8 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+-- Simulates source database
+
 CREATE TABLE [dbo].[Color](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](32) NULL
@@ -16,7 +18,6 @@ GO
 
 CREATE TABLE [dbo].[Shirt](
 	[Id] [int] NOT NULL,
-	[Color] [nvarchar](50) NULL,
 	[Size] [int] NULL,
 	[Color_Id] [int] NULL
 ) ON [PRIMARY]
@@ -31,3 +32,36 @@ REFERENCES [dbo].[Color] ([Id])
 GO
 ALTER TABLE [dbo].[Shirt] CHECK CONSTRAINT [FK_Shirt_Color]
 GO
+
+-- Datavault (limited implementation, hub and satellite tables only)
+
+CREATE TABLE [dbo].[H_Color](
+	[Id] [int] NOT NULL,
+	[Code] [nvarchar](2) NOT NULL,
+	[Timestamp] [datetime] NOT NULL,
+	[Source] [nvarchar](32) NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[H_Color] ADD  CONSTRAINT [PK_H_Color] PRIMARY KEY NONCLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+
+GO
+CREATE TABLE [dbo].[S_Color](
+	[Color_Id] [int] NOT NULL,
+	[Name] [nvarchar](32) NOT NULL,
+	[Timestamp] [datetime] NOT NULL,
+	[Source] [nvarchar](32) NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[S_Color] ADD  CONSTRAINT [PK_S_Color] PRIMARY KEY CLUSTERED 
+(
+	[Color_Id] ASC,
+	[Timestamp] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+
+
+
